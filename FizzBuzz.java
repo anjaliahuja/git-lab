e83950c03141c500a917ac5b3bc16a9387a92241
alfredkoshan <alfred.koshan@gmail.com>
2013-10-29 10:27:32 -0700

    Added rule for Fizz

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,7 +4,8 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			for(int i=0; i<100; i++) {
-				System.out.println(i);
+				if( i % 3 ==0)
+					System.out.println(i + ":" + "Fizz");
 			}
 	}
 
